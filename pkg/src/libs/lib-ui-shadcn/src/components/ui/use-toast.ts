20719379e00b04c2ddf104619e41d93e316eefd2
Ying Wang <yingw787.personal@gmail.com>
2024-01-12 15:27:45 -0500

    Update lib-ui-shadcn

--- pkg/src/libs/lib-ui-shadcn/src/components/ui/use-toast.ts
+++ pkg/src/libs/lib-ui-shadcn/src/components/ui/use-toast.ts
@@ -84,7 +84,7 @@ export const reducer = (state: State, action: Action): State => {
       return {
         ...state,
         toasts: state.toasts.map((t) =>
-          t.id === action.toast.id ? { ...t, ...action.toast } : t
+          t.id === action.toast.id ? { ...t, ...action.toast } : t,
         ),
       };
 
@@ -109,7 +109,7 @@ export const reducer = (state: State, action: Action): State => {
                 ...t,
                 open: false,
               }
-            : t
+            : t,
         ),
       };
     }
