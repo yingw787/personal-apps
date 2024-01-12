cef59301efe7f270c4a8bb6c33ea644b06bf079b
Ying Wang <yingw787.personal@gmail.com>
2024-01-12 11:52:07 -0500

    Update layout.tsx

--- pkg/src/apps/com-yingw787/app/layout.tsx
+++ pkg/src/apps/com-yingw787/app/layout.tsx
@@ -1,9 +1,9 @@
 import "$/global.css";
 
 import type { Metadata } from "next";
-import { Playfair_Display } from "next/font/google";
+import { Inter } from "next/font/google";
 
-const playfair_display = Playfair_Display({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 // The html <head> title and description metadata.
 export const metadata: Metadata = {
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       {/* The head should be programmatically generated using the NextJS metadata API - see: https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons */}
       <link rel="icon" href="/images/logo_cat.jpg" sizes="any" />
-      <body className={playfair_display.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
