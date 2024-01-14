694d211fbc12260141385c35fb243e24267681e9
Ying Wang <yingw787.personal@gmail.com>
2024-01-13 23:40:02 -0500

    Update header.tsx

--- pkg/src/apps/com-yingw787/components/header.tsx
+++ pkg/src/apps/com-yingw787/components/header.tsx
@@ -133,13 +133,13 @@ const DesktopNavigation = (props: React.ComponentPropsWithoutRef<"nav">) => {
                 className={clsx(
                   "relative block px-3 py-2 transition",
                   isActive
-                    ? "text-teal-500 dark:text-teal-400"
-                    : "hover:text-teal-500 dark:hover:text-teal-400",
+                    ? "text-brand-primary-500 dark:text-brand-primary-400"
+                    : "hover:text-brand-primary-500 dark:hover:text-brand-primary-400",
                 )}
               >
                 {link.title}
                 {isActive && (
-                  <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+                  <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-brand-primary-500/0 via-brand-primary-500/40 to-brand-primary-500/0 dark:from-brand-primary-400/0 dark:via-brand-primary-400/40 dark:to-brand-primary-400/0" />
                 )}
               </Link>
             </li>
@@ -166,8 +166,8 @@ const ThemeToggle = () => {
       className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
       onClick={() => setTheme(otherTheme)}
     >
-      <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
-      <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
+      <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-brand-primary-50 [@media(prefers-color-scheme:dark)]:stroke-brand-primary-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-brand-primary-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-brand-primary-600" />
+      <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-brand-primary-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-brand-primary-500" />
     </button>
   );
 };
