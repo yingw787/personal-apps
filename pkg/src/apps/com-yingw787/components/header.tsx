33f966e35665149f4fbce28a19d33eef0266f94e
Ying Wang <yingw787.personal@gmail.com>
2024-01-13 23:34:57 -0500

    Update header.tsx

--- pkg/src/apps/com-yingw787/components/header.tsx
+++ pkg/src/apps/com-yingw787/components/header.tsx
@@ -103,44 +103,48 @@ const MobileNavigation = (
   );
 };
 
-const NavItem = ({
-  href,
-  children,
-}: {
-  href: string;
-  children: React.ReactNode;
-}) => {
-  const isActive = usePathname() === href;
-
-  return (
-    <li>
-      <Link
-        href={href}
-        className={clsx(
-          "relative block px-3 py-2 transition",
-          isActive
-            ? "text-teal-500 dark:text-teal-400"
-            : "hover:text-teal-500 dark:hover:text-teal-400",
-        )}
-      >
-        {children}
-        {isActive && (
-          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
-        )}
-      </Link>
-    </li>
-  );
-};
-
 const DesktopNavigation = (props: React.ComponentPropsWithoutRef<"nav">) => {
+  const currentPath = usePathname();
+
+  const links = [
+    {
+      href: ROUTE_ABOUT,
+      title: "About",
+    },
+    {
+      href: ROUTE_PROJECTS,
+      title: "Projects",
+    },
+    {
+      href: ROUTE_TOOLCHAIN,
+      title: "Toolchain",
+    },
+  ];
+
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/about">About</NavItem>
-        <NavItem href="/articles">Articles</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/speaking">Speaking</NavItem>
-        <NavItem href="/uses">Uses</NavItem>
+        {links.map((link, index) => {
+          const isActive = currentPath === link.href;
+          return (
+            <li key={index}>
+              <Link
+                href={link.href}
+                className={clsx(
+                  "relative block px-3 py-2 transition",
+                  isActive
+                    ? "text-teal-500 dark:text-teal-400"
+                    : "hover:text-teal-500 dark:hover:text-teal-400",
+                )}
+              >
+                {link.title}
+                {isActive && (
+                  <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+                )}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
