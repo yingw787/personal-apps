57931f18e6137ad70eb98138a5871a3267763f6e
Ying Wang <yingw787.personal@gmail.com>
2024-01-13 23:28:57 -0500

    Update header.tsx changing icons to heroicons and add links.

--- pkg/src/apps/com-yingw787/components/header.tsx
+++ pkg/src/apps/com-yingw787/components/header.tsx
@@ -7,94 +7,39 @@ import { usePathname } from "next/navigation";
 import { useTheme } from "next-themes";
 import { Popover, Transition } from "@headlessui/react";
 import clsx from "clsx";
+import {
+  XMarkIcon,
+  ChevronDownIcon,
+  SunIcon,
+  MoonIcon,
+} from "@heroicons/react/24/outline";
 
 import { Container } from "@apps/com-yingw787/components/container";
 import avatarImage from "@apps/com-yingw787/public/images/logo_cat.jpg";
-
-// TODO: Remove
-const CloseIcon = (props: React.ComponentPropsWithoutRef<"svg">) => {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
-
-// TODO: Remove
-const ChevronDownIcon = (props: React.ComponentPropsWithoutRef<"svg">) => {
-  return (
-    <svg viewBox="0 0 8 6" aria-hidden="true" {...props}>
-      <path
-        d="M1.75 1.75 4 4.25l2.25-2.5"
-        fill="none"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
-
-// TODO: Remove
-const SunIcon = (props: React.ComponentPropsWithoutRef<"svg">) => {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z" />
-      <path
-        d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
-        fill="none"
-      />
-    </svg>
-  );
-};
-
-// TODO: Remove
-const MoonIcon = (props: React.ComponentPropsWithoutRef<"svg">) => {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        d="M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
-
-const MobileNavItem = ({
-  href,
-  children,
-}: {
-  href: string;
-  children: React.ReactNode;
-}) => {
-  return (
-    <li>
-      <Popover.Button as={Link} href={href} className="block py-2">
-        {children}
-      </Popover.Button>
-    </li>
-  );
-};
+import {
+  ROUTE_ABOUT,
+  ROUTE_PROJECTS,
+  ROUTE_TOOLCHAIN,
+} from "@apps/com-yingw787/lib/routes";
 
 const MobileNavigation = (
   props: React.ComponentPropsWithoutRef<typeof Popover>,
 ) => {
+  const links = [
+    {
+      href: ROUTE_ABOUT,
+      title: "About",
+    },
+    {
+      href: ROUTE_PROJECTS,
+      title: "Projects",
+    },
+    {
+      href: ROUTE_TOOLCHAIN,
+      title: "Toolchain",
+    },
+  ];
+
   return (
     <Popover {...props}>
       <Popover.Button className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
@@ -128,7 +73,7 @@ const MobileNavigation = (
           >
             <div className="flex flex-row-reverse items-center justify-between">
               <Popover.Button aria-label="Close menu" className="-m-1 p-1">
-                <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
+                <XMarkIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
               </Popover.Button>
               <h2 className="text-sm font-medium text-zinc-600 dark:text-zinc-400">
                 Navigation
@@ -136,11 +81,19 @@ const MobileNavigation = (
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                <MobileNavItem href="/about">About</MobileNavItem>
-                <MobileNavItem href="/articles">Articles</MobileNavItem>
-                <MobileNavItem href="/projects">Projects</MobileNavItem>
-                <MobileNavItem href="/speaking">Speaking</MobileNavItem>
-                <MobileNavItem href="/uses">Uses</MobileNavItem>
+                {links.map((link, index) => {
+                  return (
+                    <li key={index}>
+                      <Popover.Button
+                        as={Link}
+                        href={link.href}
+                        className="block py-2"
+                      >
+                        {link.title}
+                      </Popover.Button>
+                    </li>
+                  );
+                })}
               </ul>
             </nav>
           </Popover.Panel>
