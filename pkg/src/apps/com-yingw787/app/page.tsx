d1955bef83cf874e2123b3eabc518a286778735a
Ying Wang <yingw787.personal@gmail.com>
2024-01-14 16:53:59 -0500

    Update /homepage.

--- pkg/src/apps/com-yingw787/app/page.tsx
+++ pkg/src/apps/com-yingw787/app/page.tsx
@@ -274,13 +274,12 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software designer, founder, and amateur astronaut.
+            The only thing I know is that I know nothing.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            My name&apos;s Ying. I build quality products that make
+            people&apos;s lives better. When I&apos;m not working, I enjoy
+            powerlifting, reading, and trying new things.
           </p>
           <div className="mt-6 flex gap-6">
             {/* <SocialLink
