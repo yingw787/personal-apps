e59a2832567d6c13574cce87a5edd009864e4146
Ying Wang <yingw787.personal@gmail.com>
2024-01-15 00:03:04 -0500

    Update.

---
+++ pkg/src/apps/com-yingw787-blog/mdx.d.ts
@@ -0,0 +1,7 @@
+// mdx.d.ts
+declare module "*.mdx" {
+  let MDXComponent: (
+    props: React.ComponentPropsWithoutRef<"div"> & Record<string, unknown>,
+  ) => JSX.Element;
+  export default MDXComponent;
+}
