8a90617c80b9e3f26026805f7378c9466b4f2ebd
Ying Wang <yingw787.personal@gmail.com>
2024-01-14 22:23:14 -0500

    Update /toolchain.

--- pkg/src/apps/com-yingw787/app/toolchain/page.tsx
+++ pkg/src/apps/com-yingw787/app/toolchain/page.tsx
@@ -71,67 +71,45 @@ const Toolchain = () => {
             </p>
           </Tool>
           <Tool title="2x Dell U2717D Monitors (2018)">
-            The only display on the market if you want something HiDPI and
-            bigger than 27”. When you’re working at planetary scale, every pixel
-            you can get counts.
+            Linux resolution scaling can be a nightmare. I purchased my laptop
+            with the smallest resolution possible (1440p instead of 4K). For
+            monitors, I was worried that Linux drivers for ultrawide monitors
+            would not be supported, so opted for a more traditional setup of two
+            monitors. More screen space as a developer = higher productivity.
           </Tool>
-          <Tool title="IBM Model M SSK Industrial Keyboard">
-            They don’t make keyboards the way they used to. I buy these any time
-            I see them go up for sale and keep them in storage in case I need
-            parts or need to retire my main.
-          </Tool>
-          <Tool title="Apple Magic Trackpad">
-            Something about all the gestures makes me feel like a wizard with
-            special powers. I really like feeling like a wizard with special
-            powers.
-          </Tool>
-          <Tool title="Herman Miller Aeron Chair">
-            If I’m going to slouch in the worst ergonomic position imaginable
-            all day, I might as well do it in an expensive chair.
+          <Tool title="Herman Miller Aeron Chair (with Lumbar Support)">
+            Herman Miller chairs come new at $2000. I buy my Herman Miller
+            chairs for around $400 on Craigslist. I bought one for work and one
+            for home. I dropped the second one down some steps in D.C. and it
+            still works fine.
           </Tool>
         </ToolsSection>
         <ToolsSection title="Development tools">
-          <Tool title="Sublime Text 4">
-            I don’t care if it’s missing all of the fancy IDE features everyone
-            else relies on, Sublime Text is still the best text editor ever
-            made.
+          <Tool title="Visual Studio Code">
+            This editor is so good, it made some Microsoft haters come out of
+            their shell. Look through the extensions marketplace for popular
+            extensions, it turns a simple text editor into an IDE beast.
           </Tool>
-          <Tool title="iTerm2">
-            I’m honestly not even sure what features I get with this that aren’t
-            just part of the macOS Terminal but it’s what I use.
+          <Tool title="tmux">
+            I used to use iTerm2 but there is no equal on Linux. So I use tmux -
+            combined with tmux-resurrect, I can create the perfect terminal
+            layout for me, take it to any operating system, and save my setup to
+            disk to boot up whenever I restart my laptop.
           </Tool>
-          <Tool title="TablePlus">
-            Great software for working with databases. Has saved me from
-            building about a thousand admin interfaces for my various projects
-            over the years.
-          </Tool>
-        </ToolsSection>
-        <ToolsSection title="Design">
-          <Tool title="Figma">
-            We started using Figma as just a design tool but now it’s become our
-            virtual whiteboard for the entire company. Never would have expected
-            the collaboration features to be the real hook.
+          <Tool title="OBS Studio">
+            An open-source version of Loom. Pretty much has all features Loom
+            has, a bit clunkier to use with worse audio, but totally free.
           </Tool>
         </ToolsSection>
         <ToolsSection title="Productivity">
-          <Tool title="Alfred">
-            It’s not the newest kid on the block but it’s still the fastest. The
-            Sublime Text of the application launcher world.
+          <Tool title="ChatGPT">
+            It&apos;s pretty much my unpaid intern that I don&apos;t feel bad
+            about underpaying.
           </Tool>
-          <Tool title="Reflect">
-            Using a daily notes system instead of trying to keep things
-            organized by topics has been super powerful for me. And with
-            Reflect, it’s still easy for me to keep all of that stuff
-            discoverable by topic even though all of my writing happens in the
-            daily note.
-          </Tool>
-          <Tool title="SavvyCal">
-            Great tool for scheduling meetings while protecting my calendar and
-            making sure I still have lots of time for deep work during the week.
-          </Tool>
-          <Tool title="Focus">
-            Simple tool for blocking distracting websites when I need to just do
-            the work and get some momentum going.
+          <Tool title="Hourly Journal">
+            I built out my own software to record an hourly journal, where I
+            actually record my day in 15-minute increments. It&apos;s helped me
+            stay focused during long stretches of personal time.
           </Tool>
         </ToolsSection>
       </div>
