dcdfe74871fcaefbdbfd9a486dbc0786da3fe357
Ying Wang <yingw787.personal@gmail.com>
2024-01-16 11:44:00 -0500

    Add Homepage.

--- pkg/src/apps/com-yingw787-blog/app/page.tsx
+++ pkg/src/apps/com-yingw787-blog/app/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import { ROUTE_HOME, ROUTE_ABOUT } from "@apps/com-yingw787-blog/lib/routes";
 import { Header } from "@libs/blog/components/header";
 
 const Homepage = () => {
+  const currentPath = usePathname();
+
   return (
     <>
-      <Header />
+      <Header
+        headerTitle="Ying's Personal Blog"
+        routeHome={ROUTE_HOME}
+        navigation={[
+          {
+            href: ROUTE_ABOUT,
+            title: "About",
+            isActive: currentPath === ROUTE_ABOUT,
+          },
+        ]}
+      />
     </>
   );
 };
