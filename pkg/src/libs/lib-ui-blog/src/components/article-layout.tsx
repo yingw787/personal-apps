10852d9c9651f1aa27ba3724dd3b9c465905fadf
Ying Wang <yingw787.personal@gmail.com>
2024-01-16 18:08:28 -0500

    Remove BackButton.

--- pkg/src/libs/lib-ui-blog/src/components/article-layout.tsx
+++ pkg/src/libs/lib-ui-blog/src/components/article-layout.tsx
@@ -1,31 +1,12 @@
 "use client";
 
 import moment from "moment";
-import { useContext } from "react";
-import { useRouter } from "next/navigation";
-import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { twMerge } from "tailwind-merge";
 
-import { ApplicationContext } from "@libs/blog/contexts/application-context";
 import { Container } from "@libs/blog/components/container";
 import { Prose } from "@libs/blog/components/prose";
 import { type ArticleWithSlug } from "@libs/blog/types/articles";
 import { Theme } from "@libs/blog/types/themes";
-
-const BackButton = () => {
-  const router = useRouter();
-
-  return (
-    <button
-      type="button"
-      onClick={() => router.back()}
-      aria-label="Go back to articles"
-      className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
-    >
-      <ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
-    </button>
-  );
-};
 
 interface ArticleHeaderProps {
   article: ArticleWithSlug;
@@ -70,13 +51,10 @@ export const ArticleLayout = ({
   theme: Theme;
   children: React.ReactNode;
 }) => {
-  const { previousPathname } = useContext(ApplicationContext);
-
   return (
     <Container className="mt-16 lg:mt-32">
       <div className="xl:relative">
         <div className="mx-auto max-w-2xl">
-          {previousPathname && <BackButton />}
           <article>
             <ArticleHeader article={article} theme={theme} />
             <Prose className="mt-8" data-mdx-content>
