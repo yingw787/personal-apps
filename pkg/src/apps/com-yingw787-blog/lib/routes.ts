c2eec9956860326292e5760590a9d9a952a40b2b
Ying Wang <yingw787.personal@gmail.com>
2024-01-16 11:40:34 -0500

    Add lib/routes.ts.

---
+++ pkg/src/apps/com-yingw787-blog/lib/routes.ts
@@ -0,0 +1,7 @@
+export const ROUTE_HOME = "/";
+export const ROUTE_POSTS = "/posts";
+export const ROUTE_ABOUT = "/about";
+
+export const ALL_ROUTES = [ROUTE_HOME, ROUTE_POSTS, ROUTE_ABOUT];
+
+export const ROUTE_REDIRECT_TO = ROUTE_HOME;
