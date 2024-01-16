c08d653fdf979b0797cfba0eb1d0228237aca064
Ying Wang <yingw787.personal@gmail.com>
2024-01-16 17:45:22 -0500

    Update next.config.js adding mdxRs compiler.

--- pkg/src/apps/com-yingw787-blog/next.config.js
+++ pkg/src/apps/com-yingw787-blog/next.config.js
@@ -27,6 +27,9 @@ const nextConfig = {
   },
   // Configure pageExtensions to include md and mdx
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
   // Optionally, add any other Next.js config below
   reactStrictMode: true,
 };
