7b6a428ca4ca53f8c0d324341f4d9b194281cf68
Ying Wang <yingw787.personal@gmail.com>
2024-01-16 11:44:26 -0500

    Update layout.tsx

--- pkg/src/apps/com-yingw787-blog/app/layout.tsx
+++ pkg/src/apps/com-yingw787-blog/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
       <body
         className={twMerge(
-          "flex h-full bg-base3 dark:bg-base03",
+          "flex flex-col min-h-[100vh] bg-base3 dark:bg-base03",
           ibmPlexMono.className,
         )}
       >
