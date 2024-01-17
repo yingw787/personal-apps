f9cc7f26090d91b6549d68b28d7b0cd27a3b7421
Ying Wang <yingw787.personal@gmail.com>
2024-01-17 11:10:56 -0500

    Add posts.ts and navigation.ts

---
+++ pkg/src/apps/com-yingw787-bytes/lib/posts.ts
@@ -0,0 +1,22 @@
+import glob from "fast-glob";
+
+import { Article, ArticleWithSlug } from "@libs/blog/types/articles";
+
+async function importPost(postFilename: string): Promise<ArticleWithSlug> {
+  const post = await import(
+    `@apps/com-yingw787-bytes/app/posts/${postFilename}`
+  );
+  const postWithSlug = {
+    slug: postFilename.replace(/(\/page)?\.mdx$/, ""),
+    ...(post.article as Article),
+  };
+  return postWithSlug;
+}
+
+export async function getAllPosts() {
+  const postFilenames = await glob("**/page.mdx", {
+    cwd: `${process.cwd()}/app/posts`,
+  });
+  const posts = await Promise.all(postFilenames.map(importPost));
+  return posts.sort((a, z) => +new Date(z.date) - +new Date(a.date));
+}
