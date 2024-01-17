6683f019429b7e21cf80c6c12ed37c242b5a4289
Ying Wang <yingw787.personal@gmail.com>
2024-01-16 21:40:56 -0500

    Update /homepage.

--- pkg/src/apps/com-yingw787-blog/app/page.tsx
+++ pkg/src/apps/com-yingw787-blog/app/page.tsx
@@ -12,6 +12,21 @@ import { SocialLinksSection } from "@libs/blog/components/social-links";
 import { SocialLinkNavigation } from "@libs/blog/types/navigation";
 import portraitImage from "@apps/com-yingw787-blog/public/images/picture_kate_sleeping.jpg";
 
+const InspirationalQuote = () => {
+  return (
+    <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-base3 dark:border-gray-500 dark:bg-base03">
+    <p className="text-lg italic font-bold leading-relaxed text-gray-900 dark:text-white">
+      &quot;You don&apos;t ever have to do anything sensational for
+      people to love you.&quot;
+    </p>
+    <br />
+    <p className="text-md font-medium leading-relaxed text-gray-900 dark:text-white">
+      Fred Rogers, Dartmouth College commencement address, 2002
+    </p>
+  </blockquote>
+  )
+}
+
 const Home = () => {
   const socialLinks: SocialLinkNavigation[] = [
     {
@@ -55,16 +70,38 @@ const Home = () => {
           </div>
         </div>
         <div className="lg:order-first lg:row-span-2">
-          <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
-            <p className="text-2xl italic font-bold leading-relaxed text-gray-900 dark:text-white">
-              &quot;You don&apos;t ever have to do anything sensational for
-              people to love you.&quot;
+          <InspirationalQuote />
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>
+              I’ve loved making things for as long as I can remember, and wrote
+              my first program when I was 6 years old, just two weeks after my
+              mom brought home the brand new Macintosh LC 550 that I taught
+              myself to type on.
             </p>
-            <br />
-            <p className="text-xl font-medium leading-relaxed text-gray-900 dark:text-white">
-              Fred Rogers, Dartmouth College commencement address, 2002
+            <p>
+              The only thing I loved more than computers as a kid was space.
+              When I was 8, I climbed the 40-foot oak tree at the back of our
+              yard while wearing my older sister’s motorcycle helmet, counted
+              down from three, and jumped — hoping the tree was tall enough that
+              with just a bit of momentum I’d be able to get to orbit.
             </p>
-          </blockquote>
+            <p>
+              I spent the next few summers indoors working on a rocket design,
+              while I recovered from the multiple surgeries it took to fix my
+              badly broken legs. It took nine iterations, but when I was 15 I
+              sent my dad’s Blackberry into orbit and was able to transmit a
+              photo back down to our family computer from space.
+            </p>
+            <p>
+              Today, I’m the founder of Planetaria, where we’re working on
+              civilian space suits and manned shuttle kits you can assemble at
+              home so that the next generation of kids really <em>can</em> make
+              it to orbit — from the comfort of their own backyards.
+            </p>
+          </div>
+
+
+
         </div>
         <div className="lg:pl-20">
           <SocialLinksSection
