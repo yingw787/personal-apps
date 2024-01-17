7af31e52c165e1edff87ef882f82548e65919c7c
Ying Wang <yingw787.personal@gmail.com>
2024-01-17 17:39:41 -0500

    Update article.tsx add FullArticle and page.tsx refactor.

--- pkg/src/libs/lib-ui-blog/src/components/article.tsx
+++ pkg/src/libs/lib-ui-blog/src/components/article.tsx
@@ -19,3 +19,34 @@ export const RecentArticle = ({ article }: RecentArticleProps) => {
     </Card>
   );
 };
+
+interface FullArticleProps {
+  article: ArticleWithSlug;
+}
+
+export const FullArticle = ({ article }: FullArticleProps) => {
+  return (
+    <article className="md:grid md:grid-cols-4 md:items-baseline">
+      <Card className="md:col-span-3">
+        <Card.Title href={`/posts/${article.slug}`}>{article.title}</Card.Title>
+        <Card.Eyebrow
+          as="time"
+          dateTime={article.date}
+          className="md:hidden"
+          decorate
+        >
+          {moment(article.date).utc().format("MMMM D, YYYY")}
+        </Card.Eyebrow>
+        <Card.Description>{article.description}</Card.Description>
+        <Card.Cta>Read article</Card.Cta>
+      </Card>
+      <Card.Eyebrow
+        as="time"
+        dateTime={article.date}
+        className="mt-1 hidden md:block"
+      >
+        {moment(article.date).utc().format("MMMM D, YYYY")}
+      </Card.Eyebrow>
+    </article>
+  );
+}
