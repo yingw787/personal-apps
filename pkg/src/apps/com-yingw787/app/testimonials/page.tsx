32225c4f6b0a512f279375a5a95d481f2b4aa674
Ying Wang <yingw787.personal@gmail.com>
2024-02-29 18:40:21 -0500

    Update testimonials.

--- pkg/src/apps/com-yingw787/app/testimonials/page.tsx
+++ pkg/src/apps/com-yingw787/app/testimonials/page.tsx
@@ -6,9 +6,9 @@ import { SimpleLayout } from "@libs/blog/components/simple-layout";
 import MikeEshelman from "@apps/com-yingw787/public/images/picture_mike_eshelman.jpeg";
 
 interface TestimonialSectionProps {
-  quote: string;
+  quote: React.ReactNode;
   avatarImage: StaticImageData;
-  name: string;
+  name: React.ReactNode;
   role: React.ReactNode;
 }
 
@@ -22,7 +22,7 @@ const TestimonialSection = ({
     <section className="bg-white px-6 py-6 sm:py-8 lg:px-4">
       <figure className="mx-auto max-w-2xl">
         <p className="sr-only">5 out of 5 stars</p>
-        <div className="flex gap-x-1 text-brand-primary-500">
+        <div className="flex gap-x-1 text-green-500">
           <StarIcon className="h-5 w-5 flex-none" aria-hidden="true" />
           <StarIcon className="h-5 w-5 flex-none" aria-hidden="true" />
           <StarIcon className="h-5 w-5 flex-none" aria-hidden="true" />
@@ -56,20 +56,48 @@ export const metadata = {
 };
 
 type TestimonialQuote = {
-  quote: string;
+  quote: React.ReactNode;
   avatarImage: StaticImageData;
-  name: string;
+  name: React.ReactNode;
   role: React.ReactNode;
 };
 
 const Testimonials = () => {
   const testimonials: TestimonialQuote[] = [
     {
-      quote:
-        "Ying is an incredible talent. The first time we met in Q3 2023 I had mentioned a few things about the mortgage industry and how consumers had limited access to understanding a certain topic, within 3 days he had a working OpenAI ChatGPT persona trained and running on a site that blew me away. He has an entrepreneurial spirit, strong work ethic, and a desire to continue learning which is a fantastic recipe for success.",
+      quote: (
+        <>
+          Ying is an incredible talent. The first time we met in Q3 2023, I had
+          mentioned a few things about the mortgage industry, and how consumers
+          had limited access to understanding a certain topic. Within 3 days, he
+          had a working OpenAI ChatGPT persona trained and running on a site
+          that blew me away. He has an entrepreneurial spirit, strong work
+          ethic, and a desire to continue learning which is a fantastic recipe
+          for success.
+        </>
+      ),
       avatarImage: MikeEshelman,
-      name: "Mike Eshelman",
-      role: <>fmr. CMO of Mr.Cooper (NASDAQ: $COOP)</>,
+      name: (
+        <>
+          <a
+            href="https://www.linkedin.com/in/eshelman"
+            className="text-blue-600"
+          >
+            Mike Eshelman
+          </a>
+        </>
+      ),
+      role: (
+        <>
+          fmr. CMO of Mr.Cooper{" "}
+          <a
+            href="https://finance.yahoo.com/quote/COOP"
+            className="text-blue-600 font-semibold"
+          >
+            (NASDAQ: $COOP)
+          </a>
+        </>
+      ),
     },
   ];
 
