0cae67a3204478546d5bfa722b58670b09f0feac
Ying Wang <yingw787.personal@gmail.com>
2024-02-29 10:01:10 -0500

    Running setup-tailwind for com-yingw787-cooking

---
+++ pkg/src/apps/com-yingw787-cooking/tailwind.config.js
@@ -0,0 +1,101 @@
+const { createGlobPatternsForDependencies } = require("@nx/react/tailwind");
+const { join } = require("path");
+
+/** @type {import('tailwindcss').Config} */
+
+const theme_colors = {
+  // Color gradients generated from https://uicolors.app/create
+
+  // Primary color: Cerise Red
+  'brand-primary': '#e63f66',
+  'brand-primary-50': '#fef2f4',
+  'brand-primary-100': '#fde6e9',
+  'brand-primary-200': '#fbd0d9',
+  'brand-primary-300': '#f7aab9',
+  'brand-primary-400': '#f27a93',
+  'brand-primary-500': '#e63f66',
+  'brand-primary-600': '#d42a5b',
+  'brand-primary-700': '#b21e4b',
+  'brand-primary-800': '#951c45',
+  'brand-primary-900': '#801b40',
+  'brand-primary-950': '#470a1f',
+};
+
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    join(
+      __dirname,
+      "{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}",
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      backgroundColor: (_) => theme_colors,
+      colors: {
+        ...theme_colors,
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")],
+};
