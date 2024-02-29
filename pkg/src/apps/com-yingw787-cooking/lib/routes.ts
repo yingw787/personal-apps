d6967aaf4bea23b84d583307a7b4d32597a3bc26
Ying Wang <yingw787.personal@gmail.com>
2024-02-29 09:23:49 -0500

    Add routes.ts

---
+++ pkg/src/apps/com-yingw787-cooking/lib/routes.ts
@@ -0,0 +1,7 @@
+export const ROUTE_HOME = "/";
+export const ROUTE_ABOUT = "/about";
+export const ROUTE_RECIPES = "/recipes";
+
+export const ALL_ROUTES = [ROUTE_HOME, ROUTE_RECIPES, ROUTE_ABOUT];
+
+export const ROUTE_REDIRECT_TO = ROUTE_HOME;
