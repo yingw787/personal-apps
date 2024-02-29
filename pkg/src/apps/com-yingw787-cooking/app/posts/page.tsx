c7f55ccdf624092cb6a8b84b251d8328d983bae6
Ying Wang <yingw787.personal@gmail.com>
2024-02-29 10:38:57 -0500

    Add /posts.

---
+++ pkg/src/apps/com-yingw787-cooking/app/posts/page.tsx
@@ -0,0 +1,26 @@
+import { type Metadata } from "next";
+
+import { SimpleLayout } from "@libs/blog/components/simple-layout";
+import { FullArticle } from "@libs/blog/components/article";
+import { getAllPosts } from "@apps/com-yingw787-cooking/lib/posts";
+
+export const metadata: Metadata = {
+  title: "Posts",
+  description: null,
+};
+
+export default async function PostsIndex() {
+  const articles = await getAllPosts();
+
+  return (
+    <SimpleLayout title={null} intro={null}>
+      <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+        <div className="flex max-w-3xl flex-col space-y-16">
+          {articles.map((article, index) => (
+            <FullArticle key={index} article={article} />
+          ))}
+        </div>
+      </div>
+    </SimpleLayout>
+  );
+}
