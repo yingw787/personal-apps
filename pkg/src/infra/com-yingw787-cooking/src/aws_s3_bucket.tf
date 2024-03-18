b0dfd05fc5c014bef75315273b0809d3142d9692
Ying Wang <yingw787.personal@gmail.com>
2024-03-18 16:14:14 -0400

    terraform fmt update aws_s3_bucket.tf

--- pkg/src/infra/com-yingw787-cooking/src/aws_s3_bucket.tf
+++ pkg/src/infra/com-yingw787-cooking/src/aws_s3_bucket.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket_ownership_controls" "website_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "website_bucket" {
-  bucket = aws_s3_bucket.static_website.id
+  bucket                  = aws_s3_bucket.static_website.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
