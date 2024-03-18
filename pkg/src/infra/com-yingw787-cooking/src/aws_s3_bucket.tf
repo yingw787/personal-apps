6eb41b416a3f40c173fb79b4fe8b55031fef1d18
Ying Wang <yingw787.personal@gmail.com>
2024-03-18 15:57:15 -0400

    Update aws_s3_bucket.tf adding website configuration for S3 bucket.

--- pkg/src/infra/com-yingw787-cooking/src/aws_s3_bucket.tf
+++ pkg/src/infra/com-yingw787-cooking/src/aws_s3_bucket.tf
@@ -3,3 +3,13 @@
 resource "aws_s3_bucket" "static_website" {
   bucket = var.domain_name
 }
+
+resource "aws_s3_bucket_website_configuration" "website_bucket" {
+  bucket = aws_s3_bucket.static_website.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+}
