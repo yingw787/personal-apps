7a01c219f882e4de64b0e4fb6fc32890a393929d
Ying Wang <yingw787.personal@gmail.com>
2024-03-18 16:09:47 -0400

    Update aws_s3_bucket.tf add ACL definitions

--- pkg/src/infra/com-yingw787-cooking/src/aws_s3_bucket.tf
+++ pkg/src/infra/com-yingw787-cooking/src/aws_s3_bucket.tf
@@ -13,3 +13,28 @@ resource "aws_s3_bucket_website_configuration" "website_bucket" {
     key = "index.html"
   }
 }
+
+# S3 bucket access control list (ACL) definitions.
+resource "aws_s3_bucket_ownership_controls" "website_bucket" {
+  bucket = aws_s3_bucket.static_website.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.static_website.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "website_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.website_bucket,
+    aws_s3_bucket_public_access_block.website_bucket,
+  ]
+  bucket = aws_s3_bucket.static_website.id
+  acl    = "private"
+}
