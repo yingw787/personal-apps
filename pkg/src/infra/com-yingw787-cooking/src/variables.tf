de100d7b736b7703259e96366d883ff7c0080ffb
Ying Wang <yingw787.personal@gmail.com>
2024-03-18 17:42:45 -0400

    Update variables.tf tfvars.example hosted_zone_id

--- pkg/src/infra/com-yingw787-cooking/src/variables.tf
+++ pkg/src/infra/com-yingw787-cooking/src/variables.tf
@@ -15,3 +15,8 @@ variable "custom_acm_certificate_arn" {
   type        = string
   description = "The Amazon Resource Number (ARN) for the wildcard SSL certificate *yingw787.com already provisioned via AWS Certificate Manager (ACM). No default as this should be set via .tfvars only!"
 }
+
+variable "hosted_zone_id" {
+  type        = string
+  description = "The ID of the AWS Route 53 hosted zone for yingw787.com."
+}
