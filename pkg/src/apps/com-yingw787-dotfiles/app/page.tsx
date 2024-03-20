4471b7f88be7dfccd229754e477eab58fcbc89e7
Ying Wang <yingw787.personal@gmail.com>
2024-03-20 11:54:38 -0400

    Add com-yingw787-dotfiles

---
+++ pkg/src/apps/com-yingw787-dotfiles/app/page.tsx
@@ -0,0 +1,12 @@
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.none file.
+   */
+  return (
+    <>
+    <div>hi</div>
+    </>
+  );
+}
