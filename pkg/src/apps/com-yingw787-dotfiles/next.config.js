e7777046750ea25766e3a9ae1d6d0fe2ea7f369e
Ying Wang <yingw787.personal@gmail.com>
2024-03-20 12:38:51 -0400

    Update next.config.js add key reactStrictMode

--- pkg/src/apps/com-yingw787-dotfiles/next.config.js
+++ pkg/src/apps/com-yingw787-dotfiles/next.config.js
@@ -7,12 +7,16 @@ const { composePlugins, withNx } = require("@nx/next");
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  output: "export", // Create a Next.js static build for deployment on AWS S3.
+  // Create a Next.js static build for deployment on AWS S3 + CloudFront + Route
+  // 53 via Terraform and `pkg-src-infra-com-yingw787-dotfiles`.
+  output: "export",
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  // Optionally, add any other Next.js config below
+  reactStrictMode: true,
 };
 
 const plugins = [
